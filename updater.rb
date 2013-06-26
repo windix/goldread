b5f83f01f893564042e95889cb8e7497a41f3da2
Wei Feng <windix@gmail.com>
2013-06-27 00:45:34 +1000

    add retry for fetching price

--- updater.rb
+++ updater.rb
@@ -9,11 +9,12 @@ module FreeKindleCN
         # mobile chrome
         client = HTTPClient.new :agent_name => "Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en-gb) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3"
 
-        content = client.get("http://www.amazon.cn/gp/aw/d/#{asin}").content
-
-        doc = Nokogiri::HTML(content, nil, 'UTF-8')
-
+        retry_times = 0
         begin
+          content = client.get("http://www.amazon.cn/gp/aw/d/#{asin}").content
+
+          doc = Nokogiri::HTML(content, nil, 'UTF-8')
+
           if doc.css('p.infoText').text == '该商品目前无法进行购买'
             book_price = kindle_price = -1
           else
@@ -23,13 +24,19 @@ module FreeKindleCN
             book_price = (book_price * 100).to_i
             kindle_price = (kindle_price * 100).to_i
           end
+        rescue Exception # => e
+          retry_times += 1
 
-          [ book_price, kindle_price ]
-        rescue Exception
-          puts content
-
-          raise
+          if retry_times > 3
+            raise
+          else
+            puts "[#{retry_times}] Exception: #{$!}"
+            sleep 5
+            retry
+          end
         end
+
+        [ book_price, kindle_price ]
       end
 
       def fetch_info(asins)
