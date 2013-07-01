a97e75e20b86f41f8f5826e242d7d8705b29715e
Wei Feng <windix@gmail.com>
2013-07-02 01:28:47 +1000

    fully working sina weibo picture uploading

--- weibo.rb
+++ weibo.rb
@@ -1,38 +1,19 @@
-require 'weibo_2'
-
-WeiboOAuth2::Config.api_key = '***REMOVED***'
-WeiboOAuth2::Config.api_secret = '***REMOVED***'
-WeiboOAuth2::Config.redirect_uri = 'http://127.0.0.1:9292/oauth/callback/weibo' # TODO
+# encoding: UTF-8
+require './weibo_config'
 
 client = WeiboOAuth2::Client.new
 
-# puts client.authorize_url
+client.get_token_from_hash(WEIBO_CONFIG)
 
-=begin
-access_token = client.auth_code.get_token('db41db51bb60b7372b3892c71bfff457')
+# client.statuses.update("Test!")
 
-p access_token.inspect
+file_path = '/Users/wfeng/Documents/41-FpgWe5OL.jpg'
 
-p access_token.params["uid"]
-p access_token.token
-p access_token.expires_at
-=end
+file_info = {
+  :filename => File.basename(file_path),
+  :type => 'image/jpeg',
+  :name => 'file',
+}
 
-#<OAuth2::AccessToken:0x007f92242d3080>
+client.statuses.upload('Test Picture', File.open(file_path), file_info)
 
-token = "2.00CuFjqDCUdT6B3a05204a6fRlqQ5C"
-expires_at = 1371495600
-
-access_token = client.get_token_from_hash(:access_token => token, :expires_at => expires_at)
-
-p access_token.inspect
-
-p access_token.params["uid"]
-p access_token.token
-p access_token.expires_at
-
-statuses = client.statuses
-
-p statuses
-
-statuses.update("Test!")
