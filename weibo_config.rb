796e3fdbfeda4b7c8d40ee09e412635a562feb73
windix@ca2 <windix@gmail.com>
2013-07-02 13:18:37 +1000

    add weibo production tokens

--- weibo_config.rb
+++ weibo_config.rb
@@ -1,11 +1,23 @@
 # encoding: UTF-8
 require 'weibo_2'
 
+# development: freekindlecn
+#WeiboOAuth2::Config.api_key = '***REMOVED***'
+#WeiboOAuth2::Config.api_secret = '***REMOVED***'
+#WeiboOAuth2::Config.redirect_uri = 'http://127.0.0.1:9292/oauth/callback/weibo'
+#
+#WEIBO_CONFIG = {
+#  :access_token => '***REMOVED***',
+#  :expires => 1372791599
+#}
+
+# production: goldreadchina
 WeiboOAuth2::Config.api_key = '***REMOVED***'
 WeiboOAuth2::Config.api_secret = '***REMOVED***'
-WeiboOAuth2::Config.redirect_uri = 'http://127.0.0.1:9292/oauth/callback/weibo' # TODO
+WeiboOAuth2::Config.redirect_uri = 'http://goldread.net/oauth/callback/weibo'
 
 WEIBO_CONFIG = {
   :access_token => '***REMOVED***',
-  :expires => 1372791599
+  :expires => 1530415035
 }
+
