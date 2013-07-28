cb55f6b3d0f9744b7f2cb8d16e7a7ae847f080e5
Wei Feng <windix@gmail.com>
2013-07-28 13:31:52 +1000

    update douban config

--- douban_config.rb
+++ douban_config.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require 'douban_api'
 
-if FreeKindleCN::CONTEXT == :production
+if false # FreeKindleCN::CONTEXT == :production
   # production: 金玉良读
 
   Douban.configure do |config|
@@ -19,14 +19,14 @@ else
   # development: 金玉良读测试
 
   Douban.configure do |config|
-    config.client_id = '0a3828a88d4ff7c61ea879f9a7efe752'
-    config.client_secret = '4f8e15984548a938'
+    config.client_id = '***REMOVED***'
+    config.client_secret = '***REMOVED***'
   end
 
   DOUBAN_CALLBACK = "http://goldread.dev/oauth/callback/douban"
-
+  
   DOUBAN_CONFIG = {
-    :access_token => '989b86dd0e8c905dc9d294972ba5b117',
+    :access_token => '3d5c8defa6bc7a69b815a9cef4356f3b',
     :user_id => ***REMOVED***
   }
 end
