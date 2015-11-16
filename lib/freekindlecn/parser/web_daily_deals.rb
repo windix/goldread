1f799dc07eb8654adf57bb5c599996d4003f96c8
Wei Feng <windix@gmail.com>
2015-11-16 22:08:01 +1100

    fix daily deal

--- lib/freekindlecn/parser/web_daily_deals.rb
+++ lib/freekindlecn/parser/web_daily_deals.rb
@@ -5,21 +5,21 @@ module FreeKindleCN
       attr_reader :daily_asins, :weekly_asins
 
       def initialize
-        @url = "http://www.amazon.cn/gp/feature.html/ref=amb_link_30648212_2?ie=UTF8&docId=126758&pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=right-top&pf_rd_t=101&pf_rd_p=70106812&pf_rd_i=116169071"
+	@url = "http://www.amazon.cn/gp/feature.html/ref=cp_bm_kindlebooksKDD?ie=UTF8&docId=126758"
       end
 
       def parse
         parse_with_retry(@url) do |doc|
           # daily deals
           @daily_asins = []
-          doc.css('div.shogun-widget').first.css('a').each do |a|
+          doc.css('div.unified_widget').first.css('div.s9OtherItems div.asin a.title').each do |a|
             asin = asin_from_url(a['href'])
             @daily_asins << asin if asin
           end
 
           # weekly deals
           @weekly_asins = []
-          doc.css('div.unified_widget').last.css('a').each do |a|
+          doc.css('div.unified_widget').last.css('div.s9OtherItems div.asin a.title').each do |a|
             asin = asin_from_url(a['href'])
             @weekly_asins << asin if asin
           end
